da8f5e1d6fcc674f4efa3614a1cc8762461769a3
Michael <madetunji1376@gmail.com>
2023-03-20 11:35:45 +0100

    routing for the app has been implemented

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './pages/login-page';
 import Dashboard from './pages/dashboard';
 import UserPage from './pages/user-page';
+import DetailsPage from './pages/details-page';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path='/' element={<Login />} />
       <Route path='/dashboard' element={<Dashboard />} />
       <Route path='/user-page' element={<UserPage />} />
+      <Route path='/details/:id' element={<DetailsPage />} />
     </Routes>
   );
 }
