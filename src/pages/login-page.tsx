b32832469ff31850d7a5c246073e7311704c5505
Michael <madetunji1376@gmail.com>
2023-03-20 23:41:18 +0100

    Login page created successfully

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import './styling/login.scss';
+import img from './assests/pablo.jpg';
+import logo from './assests/Union.png';
 
 const Login = () => {
     return (
-        <div>
-        <h1>Login Page</h1>
-        </div>
+        <section>
+            <div className='log-in'>
+                <div className='col-1'>
+                    <div className='logo-name'>
+                        <img src={logo} alt="logo" className='logo' />
+                            <h1>lendsqr</h1>
+                    </div>
+                    <img src={img} alt="image" className='img' />
+                </div>
+                <div className='col-2'>
+                    <div className="opening-text">
+                        <h2>
+                            Welcome!
+                        </h2>
+                         <p>
+                            Enter your details to login.
+                            </p>
+                    </div>
+                    <div className='form'>
+                        <form>
+                            <div className='form-group'>
+                            <input type="text" placeholder="Email" className='email' />
+                            </div>
+                            <div className='form-group-2'>
+                            <input type="password" placeholder="Password" className='password' />
+                            <span>
+                                show
+                            </span>
+                            </div>
+                        <div className='forgot-password'>
+                            <a href="#">FORGOT PASSWORD?</a>
+                        </div>
+                        <div className='login-button'>
+                            <button type="submit" className='login'>Log in</button>
+                        </div>    
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 };
 
